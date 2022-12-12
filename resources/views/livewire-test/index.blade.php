492a8a4f3b103a28e56f444d1c16a5246f400c5e
toyo09216 <atsushi09216@gmail.com>
2022-12-12 19:25:57 +0900

    livewiretest

---
+++ resources/views/livewire-test/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    @livewireStyles
+  </head>
+  <body>
+    livewireテスト
+    <div>
+      @if (session()->has('message'))
+          <div class="">
+              {{ session('message') }}
+          </div>
+      @endif
+  </div>
+
+    {{-- <livewire:counter/> --}}
+    @livewire('counter')
+
+    @livewireScripts
+  </body>
+</html>
